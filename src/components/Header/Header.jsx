63befa5e9e741660c0836bdffb7fcbc5d9fc6639
Mehedi Hasan <mhjibon.info@gmail.com>
2023-07-24 21:20:18 +0600

    component layout load data set state

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,11 +7,11 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
-                {/* a*4{href=$}{$} */}
+                {/* a*4[href=$]{$} */}
                 <a href="/order"> Order </a>
-                <a href="/review"> Order Review </a>
+                <a href="/review"> Order Review</a>
                 <a href="/inventory"> Manage Inventory </a>
-                <a href="login"> Login </a>
+                <a href="/login"> Login </a>
             </div>
         </nav>
     );
