03f918e77f65080ef2aadf69eb058c024681c823
Mehedi Hasan <mhjibon.info@gmail.com>
2023-07-25 22:15:39 +0600

    style product cart, buttom position

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -9,7 +9,11 @@ const Product = (props) => {
             <img src={img} alt="" />
             <div className='product-info'>
                 <h6 className='product-name'>{name}</h6>
+                <p> Price: ${price} </p>
+                <p> Manufacturer: {seller} </p>
+                <p> Rating: {ratings} start </p>
             </div>
+            <button className='btn-cart'> Add to Cart </button>
         </div>
     );
 };
