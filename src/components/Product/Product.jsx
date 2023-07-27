3eca1e957046db858a596aed937216711bc27c24
Mehedi Hasan <mhjibon.info@gmail.com>
2023-07-27 18:48:33 +0600

    set eventhandler and pass eventhandler to child

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,17 +3,24 @@ import './Product.css';
 
 const Product = (props) => {
     // console.log(props.product);
-    const {img, name, seller, ratings, price} = props.product;
+    // console.log(props);
+    const {img, name, price, seller, ratings} = props.product;
+    const handleAddToCart = props.handleAddToCart;
+
+    // const handleAddToCart = (product) => {
+    //     console.log(product);
+    // }
+
     return (
         <div className='product'>
             <img src={img} alt="" />
             <div className='product-info'>
                 <h6 className='product-name'>{name}</h6>
-                <p> Price: ${price} </p>
-                <p> Manufacturer: {seller} </p>
-                <p> Rating: {ratings} start </p>
+                <p> Price: ${price}</p>
+                <p> Manufacturer : {seller}</p>
+                <p> Rating : {ratings} start </p>
             </div>
-            <button className='btn-cart'> Add to Cart </button>
+            <button onClick={()=>handleAddToCart(props.product)} className='btn-cart'> Add to Cart </button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import "./Shop.css";
+import './Shop.css';
 import Product from '../Product/Product';
 
 const Shop = () => {
@@ -9,15 +9,20 @@ const Shop = () => {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
-    },[])
+    }, []);
+
+    const handleAddToCart = (product) => {
+        console.log(product);
+    }
 
     return (
         <div className='shop-container'>
             <div className='products-container'>
                 {
                     products.map(product => <Product
-                        key = {product.id}
-                        product = {product}
+                        key={product.id}
+                        product= {product}
+                        handleAddToCart={handleAddToCart}   
                     ></Product>)
                 }
             </div>
