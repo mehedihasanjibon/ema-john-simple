8185c49dd7df559fb7147cfe4cd181af83bb2f4b
Mehedi Hasan <mhjibon.info@gmail.com>
2023-07-27 19:20:35 +0600

    cart state setup and react frontawsome icon added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 
 const Product = (props) => {
@@ -20,7 +22,10 @@ const Product = (props) => {
                 <p> Manufacturer : {seller}</p>
                 <p> Rating : {ratings} start </p>
             </div>
-            <button onClick={()=>handleAddToCart(props.product)} className='btn-cart'> Add to Cart </button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
+                Add to Cart 
+                <span> <FontAwesomeIcon icon={faShoppingCart} /> </span>
+                 </button>
         </div>
     );
 };
