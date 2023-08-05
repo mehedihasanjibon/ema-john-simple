fdc9f931557f6dc44e5f52e9bb8d46b022d319a7
Mehedi Hasan <mhjibon.info@gmail.com>
2023-08-05 22:31:15 +0600

    create cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Cart.css';
 
-const Cart = (props) => {
-    const cart = props.cart; //option 1
+const Cart = ({cart}) => {
+    // const cart = props.cart; //option 1
+    // const {cart} = props; //option 2
     return (
-        <div>
+        <div className='cart'>
             <h4> Order Summary </h4>
             <p> Selected Items: {cart.length}</p>
+            <p> Total Price: $ </p>
+            <p> Total Shipping Charge: $ </p>
+            <p> Tax: $ </p>
+            <h6> Grand Total: $ </h6>
         </div>
     );
 };
