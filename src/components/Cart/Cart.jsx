19c8a797ca391a7d3c8247a5b7f6f88de69a95c9
Mehedi Hasan <mhjibon.info@gmail.com>
2023-08-07 22:39:49 +0600

    shipping and tax with toFixed

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,9 +8,15 @@ const Cart = ({cart}) => {
     console.log(cart);
 
     let totalPrice = 0;
+    let totalShipping = 0;
     for(const product of cart){
         totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
+
+    const tax = totalPrice*7/100;
+
+    const grandTotal = totalPrice + totalShipping + tax;
 
     return (
         <div className='cart'>
@@ -18,9 +24,9 @@ const Cart = ({cart}) => {
             <div>
                 <p> Selected Items: {cart.length}</p>
                 <p> Total Price: ${totalPrice} </p>
-                <p> Total Shipping Charge: $ </p>
-                <p> Tax: $ </p>
-                <h6> Grand Total: $ </h6>
+                <p> Total Shipping Charge: ${totalShipping} </p>
+                <p> Tax: ${tax.toFixed(2)} </p>
+                <h6> Grand Total: ${grandTotal.toFixed(2)} </h6>
             </div>
         </div>
     );
