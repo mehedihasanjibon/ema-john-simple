7288d9bb856ef5d68b0ac4f00786b82a933cb6ff
Mehedi Hasan <mhjibon.info@gmail.com>
2023-08-07 22:33:56 +0600

    calculate shopping cart price

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,14 +4,24 @@ import './Cart.css';
 const Cart = ({cart}) => {
     // const cart = props.cart; //option 1
     // const {cart} = props; //option 2
+
+    console.log(cart);
+
+    let totalPrice = 0;
+    for(const product of cart){
+        totalPrice = totalPrice + product.price;
+    }
+
     return (
         <div className='cart'>
-            <h4> Order Summary </h4>
-            <p> Selected Items: {cart.length}</p>
-            <p> Total Price: $ </p>
-            <p> Total Shipping Charge: $ </p>
-            <p> Tax: $ </p>
-            <h6> Grand Total: $ </h6>
+                <h4> Order Summary </h4>
+            <div>
+                <p> Selected Items: {cart.length}</p>
+                <p> Total Price: ${totalPrice} </p>
+                <p> Total Shipping Charge: $ </p>
+                <p> Tax: $ </p>
+                <h6> Grand Total: $ </h6>
+            </div>
         </div>
     );
 };
