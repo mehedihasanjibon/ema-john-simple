fc6f6841203e012fa6df24bfc0e5e5b5765572ff
Mehedi Hasan <mhjibon.info@gmail.com>
2023-08-16 03:16:30 +0600

    handle qunatity from storage to cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,13 +5,23 @@ const Cart = ({cart}) => {
     // const cart = props.cart; //option 1
     // const {cart} = props; //option 2
 
-    console.log(cart);
+    // console.log(cart);
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
+
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
+
+        // if(product.quantity === 0){
+        //     product.quantity = 1;
+        // }
+        // ::shortcut
+        product.quantity = product.quantity || 1;
+
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tax = totalPrice*7/100;
@@ -22,7 +32,8 @@ const Cart = ({cart}) => {
         <div className='cart'>
                 <h4> Order Summary </h4>
             <div>
-                <p> Selected Items: {cart.length}</p>
+                {/* <p> Selected Items: {cart.length}</p> */}
+                <p> Selected Items: {quantity}</p>
                 <p> Total Price: ${totalPrice} </p>
                 <p> Total Shipping Charge: ${totalShipping} </p>
                 <p> Tax: ${tax.toFixed(2)} </p>
